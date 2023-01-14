aa82f22ec927f9e672e44f2c532e64f6b2570de8
Dian van Schalkwyk <dian@dvsonline.co.za>
2023-01-14 19:08:04 +0000

    Removed unused constants

--- Constants.py
+++ Constants.py
@@ -12,11 +12,7 @@ PERSONAL_API = '8164746'
 
 PERSONAL_HASH = '8fe30edb11b1b7cbc31e135d7656009c'
 
-#FILE_PATH = 'C:/Users/dianv/AppData/Roaming/MetaQuotes/Terminal/AE2CC2E013FDE1E3CDF010AA51C60400/MQL5/Files/signal.txt'
-#SAVE_PATH = 'C:/Users/dianv/AppData/Roaming/MetaQuotes/Terminal/AE2CC2E013FDE1E3CDF010AA51C60400/MQL5/Files/converted.txt'
-
 FILE_PATH = 'C:/Users/dianv/AppData/Roaming/MetaQuotes/Terminal/Common/Files/signal.txt'
-#C:\Users\dianv\AppData\Roaming\MetaQuotes\Terminal\Common\Files
 
 #list of all forex pairs
 TRADE_LIST = ['AUDCAD', 'AUDCHF', 'AUDJPY', 'AUDNZD', 'AUDUSD', 'CADCHF', 'CADJPY', 'CHFJPY', 'EURAUD', 'EURCAD', 'EURCHF', 'EURGBP', 'EURJPY', 'EURNZD',
